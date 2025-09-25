cfff7e64dfd9369413223002acfdaeffba284ad5
Satya <satyabratapadhiary6@gmail.com>
2025-09-25 22:54:57 +0530

    bug fixes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('./utils/config')
 const app = require('./app')
 
-app.listen(3000,()=>{
-  console.log('Server listening at port',3000);
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+  console.log('Server listening at port',PORT);
   
 })
