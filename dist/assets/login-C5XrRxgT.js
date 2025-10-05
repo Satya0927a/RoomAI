7568d4adc18afa12a1ce230c3a9d8f9072c8645c
Satya <satyabratapadhiary6@gmail.com>
2025-10-06 00:56:02 +0530

    some changes to the ui,fixed some bugs and few changes in the prompt.json

--- dist/assets/login-C5XrRxgT.js
+++ dist/assets/login-C5XrRxgT.js
@@ -1,4 +1,4 @@
-import{r as reactExports,j as jsxRuntimeExports,c as checkverification,l as loginuser,b as createuser,s as sendpassresetlink}from"./index-B6Y67oXL.js";import{L as Loading}from"./loading-ZYm09w2s.js";/*! *****************************************************************************
+import{r as reactExports,j as jsxRuntimeExports,c as checkverification,l as loginuser,b as createuser,s as sendpassresetlink}from"./index-Bzg3PocE.js";import{L as Loading}from"./loading-BcLxCH3a.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
